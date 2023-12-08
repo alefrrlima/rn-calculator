98cd1da987390a3555ba81c908b6638372072178
Alexandre Lima <alexandrelima.beta@gmail.com>
2023-12-08 12:09:54 -0300

    theme created

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+   font: {
+      faty: {
+         regularBold: 'Roboto-Bold',
+         monoBlack: 'RobotoMono-Bold',
+      },
+      size: {
+         small: '10px',
+      },
+   },
+};
+
+export default theme;
